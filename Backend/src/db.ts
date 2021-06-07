52d52443ec7bf0178b8f3f8b3b7b6ef3c5013fda
Eric Yan <37171109+eqfy@users.noreply.github.com>
2021-06-06 23:00:12 -0700

    Refactored backend to use promises

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -1,27 +1,45 @@
-import { Pool } from "pg";
+import { Pool, QueryArrayConfig } from "pg";
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 const db = {
-  query:(text, callback) => {
-    pool.connect((err, client, release) => {
-      if (err) {
-        return console.error("Error acquiring client", err.stack);
-      }
-      client.query(text, (err, result) => {
-        release();
-        if (err) {
-          return console.error("Error executing query", err.stack);
-        }
-        callback(err, result);
-      });
-    });
+  async query(text: string | QueryArrayConfig<any[]>, params?: any[]) {
+    const start = Date.now()
+    const res = await pool.query(text, params)
+    const duration = Date.now() - start
+    console.log('executed query', { text, duration, rows: res.rowCount })
+    return res
+  },
+  async getClient() {
+    const client = await pool.connect()
+    const query = client.query
+    const release = client.release
+    // set a timeout of 5 seconds, after which we will log this client's last query
+    const timeout = setTimeout(() => {
+      console.error('A client has been checked out for more than 5 seconds!')
+      // @ts-ignore
+      console.error(`The last executed query on this client was: ${client.lastQuery}`)
+    }, 5000)
+    // monkey patch the query method to keep track of the last query executed
+    client.query = (...args) => {
+      // @ts-ignore
+      client.lastQuery = args
+      return query.apply(client, args)
+    }
+    client.release = () => {
+      // clear our timeout
+      clearTimeout(timeout)
+      // set the methods back to their old un-monkey-patched version
+      client.query = query
+      client.release = release
+      return release.apply(client)
+    }
+    return client
   }
-  };
-
+}
 
 export default db;
