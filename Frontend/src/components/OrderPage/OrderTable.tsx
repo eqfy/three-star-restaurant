79a6d836cef839df888f7802ba42d82733973a74
moseskirathe <moskirathe@gmail.com>
2021-06-11 21:10:01 -0700

    display orders

--- Frontend/src/components/OrderPage/OrderTable.tsx
+++ Frontend/src/components/OrderPage/OrderTable.tsx
@@ -1,6 +1,9 @@
 import { makeStyles } from '@material-ui/core';
 import { FunctionComponent } from 'react';
 import CustomTable from '../../common/CustomTable';
+import { GetOrders } from '../../hooks/order';
+// import { GetOrders, UpdateOrder, DeleteOrder, ProjectOrders, GetOrderCount } from '../../hooks/order';
+import { Order } from '../../types/order';
 
 const useStyles = makeStyles({
   table: {
@@ -10,17 +13,34 @@ const useStyles = makeStyles({
 
 const OrderTable: FunctionComponent = (props) => {
   const classes = useStyles();
+  const { data } = GetOrders();
+  console.log(data);
   return (
     <div className={classes.table}>
       <CustomTable
         columns={[
-          { title: 'ID', field: 'eid' },
-          { title: 'Name', field: 'name' },
-          { title: 'Role', field: 'table_name' },
-          { title: 'Employement Date', field: 'employment_date', type: 'date' },
-          { title: 'Salary', field: 'salary', type: 'currency' },
+          { title: 'ID', field: 'oid' },
+          { title: 'Status', field: 'status' },
+          { title: 'Created On', field: 'created_on', type:'date' },
+          { title: 'Waiter ID', field: 'waiter_id' },
         ]}
-        data={[]}
+        data={data}
+        title="Orders"
+        editable={{
+          onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+              //TODO: add new row
+            }),
+          onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+              // TODO: update row 
+              // UpdateOrder(((newData as Order).oid) as number, newData);  
+            }),
+          onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+              // TODO: delete row
+            }),
+        }}
       />
     </div>
   );
