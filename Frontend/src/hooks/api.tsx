6b82fee2ce8f9b95a44a02d07c3f76e46b3cd030
unknown <henrykah@students.cs.ubc.ca>
2021-06-11 18:43:27 -0700

    added backend endpoints + hooks for counting item orders

--- Frontend/src/hooks/api.tsx
+++ Frontend/src/hooks/api.tsx
@@ -32,3 +32,24 @@ export function useDishInfoOrderCount() {
     }, {});
   });
 }
+
+// Count of order_items grouped by order_id
+export function useOrderItemCount() {
+  return useQuery(['orderitemcount'], async () => {
+    const res = await fetch(`${DB_BASE_URL}/dishorderitemcount`);
+    const json = await res.json();
+    return json.reduce((acc: any, val: any) => {
+      acc[val.order_id] = val.count;
+      return acc;
+    }, {});
+  });
+}
+
+// Count of all rows in order_items
+export function useOrderItemTotalCount() {
+  return useQuery(['orderitemtotalcount'], async () => {
+    const res = await fetch(`${DB_BASE_URL}/dishorderitemtotalcount`);
+    const json = await res.json();
+    return json[0];
+  });
+}
